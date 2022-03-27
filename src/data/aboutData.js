56e03546a63aaf5ee7775b5dc857e4dcb8248b06
Srikant <srikanta@repletech.com>
2022-03-27 14:45:34 +0530

    initial commit

---
+++ src/data/aboutData.js
@@ -0,0 +1,6 @@
+export const aboutData = {
+    title: "Who I am",
+    description1: "My name's Srikant. I'm a web developer based in Bangalore, INDIA.",
+    description2: "During the day I work as a  developer at BORN GROUP . I spent my leisure hours writing articles and poetry. Right now I'm also trying a hand at Data Structure & Algorithm. I love to learn and explore new arenas.",
+    image: 2
+}
---
+++ src/data/educationData.js
@@ -0,0 +1,23 @@
+export const educationData = [
+    {
+        id: 1,
+        institution: 'Marble Hills Middle School',
+        course: 'Higher Secondary Education',
+        startYear: '2007',
+        endYear: '2015'
+    },
+    {
+        id: 2,
+        institution: 'Fakir Mohan University',
+        course: 'Bachelor of Science (IT)',
+        startYear: '2014',
+        endYear: '2017'
+    },
+    {
+        id: 3,
+        institution: 'Utkal University',
+        course: 'Master of Computer Science & Applications',
+        startYear: '2017',
+        endYear: '2020'
+    },
+]
